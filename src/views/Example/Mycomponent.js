c9938e5fa0b618c73b365ac10cd5772e57510b0c
Bach Loc Dang <intern.ldbach@tma.com.vn>
2024-04-03 10:15:19 +0700

    done #5

--- src/views/Example/Mycomponent.js
+++ src/views/Example/Mycomponent.js
@@ -1,9 +1,28 @@
 import React from "react";
 class MyComponent extends React.Component {
+    state = {
+        name: "BachDang",
+        channel: "Bachcoder"
+    }
+    handleOnchangeName = (e) => {
+        this.setState({ name: e.target.value })
+    }
     render() {
         return (
-            //code html her
-            <div>Hello My Component dangbach</div>
+            //code html here   
+            /**
+             * react.fragment
+             */
+            <>
+                <div className="first">
+
+                    My channel {this.state.channel}</div>
+                <div className="my-component">
+                    <input value={this.state.name} type="text" onChange={(e) => this.handleOnchangeName(e)} />
+                    Hello My Component. My name is {this.state.name}
+                </div>
+            </>
+
         )
     }
 }
